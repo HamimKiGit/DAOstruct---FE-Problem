66da3033490b564136bafdf0851f5a68951de6a1
Ham Mim <ehisab.india@gmail.com>
2023-02-28 08:00:26 +0530

    infinite scroll done

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,6 +3,7 @@ import { Breadcrumb, Layout, Menu, Spin, Space, Col, Row, Image, Typography } fr
 import ApiService from '../utils/ApiService';
 import { PlanetaryResponse } from '../context/interfaces';
 import HomePageSlider from './row/HomePageSlider';
+import moment from 'moment';
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -10,22 +11,29 @@ const { Title, Paragraph, Text, Link } = Typography;
 
 const HomePage = () => {
     const perChunk = 7 // items per chunk    
+    const addDay = 4
 
     const [state, setState] = React.useState<Array<PlanetaryResponse>>([])
     const [result, setResult] = React.useState<Array<Array<PlanetaryResponse>>>([])
+    const [start_date, setStartDate] = React.useState(moment().subtract(1, 'year').valueOf())
+    const [end_date, setEndDate] = React.useState(moment(start_date).add(addDay, 'day').valueOf())
+    const [loadMore, setLoadMore] = React.useState(null as any)
+
     const [ellipsis, setEllipsis] = React.useState(false)
     const [loading, setLoading] = React.useState(false)
     const uRLSearchParams = new URLSearchParams()
 
-    const initialise = async () => {
+    const initialise = async (start_date: number, end_date: number) => {
         uRLSearchParams.set("api_key", "gaff4Pwpu0Qg6woyFty1YhVRxhj4In1ImvOCyFD7")
-        uRLSearchParams.set("start_date", "2022-10-01")
-        uRLSearchParams.set("end_date", "2022-10-29")
+        uRLSearchParams.set("start_date", moment(start_date).format("YYYY-MM-DD"))
+        uRLSearchParams.set("end_date", moment(end_date).format("YYYY-MM-DD"))
         uRLSearchParams.set("thumbs", "true")
         setLoading(true)
         try {
             const apiRes = await ApiService.Planetary.apod(uRLSearchParams.toString())
-            setState(apiRes)
+            setState([...state, ...apiRes])
+            setStartDate(start_date)
+            setEndDate(end_date)
         } catch (error) {
 
         } finally {
@@ -52,8 +60,31 @@ const HomePage = () => {
         intialiseGroup()
     }, [state])
 
+
+
     React.useEffect(() => {
-        initialise()
+        if ((window.innerHeight + document.documentElement.scrollTop) + 400 >= (document?.scrollingElement?.scrollHeight || 0)) {
+            // if (state.length) {
+            if (!loading) {
+                let startDay = moment(end_date).add(1, 'day').valueOf()
+                let endDay = moment(startDay).add(addDay, 'day').valueOf()
+                if (endDay.valueOf() <= moment().valueOf()) {
+                    console.log("Loading......");
+                    initialise(startDay, endDay)
+                } else {
+                    console.log("Data ended");
+                }
+            }
+            // }
+
+        }
+    }, [loadMore])
+
+
+    React.useEffect(() => {
+        window.addEventListener("scroll", setLoadMore)
+        return () =>
+            window.removeEventListener("scroll", setLoadMore)
     }, [])
 
     return <Content style={{ margin: '0 16px' }}>
